bc5a27de028bbd4ee1c7f3ee0af6916e1f7c385a
unknown <ohaerifavour68@gmail.com>
2022-09-14 20:57:51 +0100

    Done, yes sir

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	int counter = 2;
-
 	float a = 1;
 	float b = a + 1;
 	float c = a + b;
@@ -16,8 +15,8 @@ int main(void)
 	printf("%.0f, ", b);
 	while (counter < 98)
 	{
-		counter++;
 		printf("%.0f", c);
+		counter++;
 		a = b;
 		b = c;
 		c = a + b;
